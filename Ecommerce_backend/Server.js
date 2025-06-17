08570926bc0cde65df0420728f4280ab4d21bf93
abdullah <smabdulla958>
2025-06-17 18:29:12 +0500

    add

--- Ecommerce_backend/Server.js
+++ Ecommerce_backend/Server.js
@@ -1,16 +1,21 @@
+let cors=require("cors") //for cross origin for frontend and backendd
+
 require("dotenv").config({path:".env"});
 console.log(process.env.JWT_Password);
 console.log(process.env.JWT_Expire);
 let express=require("express");
-let cors=require("cors") //for cross origin for frontend and backendd
 let cookieParser=require("cookie-parser")
 let App=express();
-App.use(cookieParser())
-App.use(express.json());
 App.use(cors({
-    origin:["http://localhost:5173","https://ecommerce-website-delta-beige.vercel.app"],
+    origin:[
+        "http://localhost:5173",
+        "https://ecommerce-website-delta-beige.vercel.app"
+    ],
     credentials:true
 }))
+
+App.use(cookieParser())
+App.use(express.json());
 const UserParentRoute = require("./Routes/USERROUTE/UserParentRoute");
 //hum different routes ko ek file mai rakh kr us ko import kr sakta hai
 //this is for a user 
