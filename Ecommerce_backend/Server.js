87f65861666e3d27f69be7f6e1a477cbd65a2055
abdullah <smabdulla958>
2025-06-18 01:17:23 +0500

    eror

--- Ecommerce_backend/Server.js
+++ Ecommerce_backend/Server.js
@@ -9,7 +9,7 @@ let App=express();
 App.use(cors({
     origin:[
         "http://localhost:5173",
-        "https://ecommerce-website-delta-beige.vercel.app"
+        process.env.React_App_Server_URL
     ],
     credentials:true
 }))
